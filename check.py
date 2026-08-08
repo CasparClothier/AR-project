c964ea31965e45827138bf220d0d83c6cd8a7624
Caspar Clothier <casparclothier@gmail.com>
2026-08-08 12:23:54 +0100

    Final test output with new denoise stage settings.

--- check.py
+++ check.py
@@ -1,5 +1,5 @@
 from pipeline2 import run_pipeline, PipelineConfig
 
 cfg = PipelineConfig(target_sr=None, routing={"type": "threshold"})
-report = run_pipeline(r"C:\Users\caspa\OneDrive\Desktop\Dsci\AR project\data\corpus_a\Ching-a-lings-jazz-bazaar_jukebox-36042_001_00-00-31.wav", "test_output2.wav", cfg)
+report = run_pipeline(r"C:\Users\caspa\OneDrive\Desktop\Dsci\AR project\data\corpus_a\Ching-a-lings-jazz-bazaar_jukebox-36042_001_00-00-31.wav", "test_output3.wav", cfg)
 print(report["decisions"])
